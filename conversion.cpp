231aa7cafb478bd36577963499db46319118c3ef
colonealcortez <72023318+colonealcortez@users.noreply.github.com>
2020-09-28 21:38:47 +0530

    Create conversion.cpp

---
+++ conversion.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int x;
+cout<<"ENETR A NUMBER:";
+cin>>x;
+cout<<"\nDECIMAL OF NUMBER:"<<dec<<x;
+cout<<"\nOCTAL OF NUMBER:"<<oct<<x;
+cout<<"\nHEXA-DECIMAL OF NUMBER:"<<hex<<x;
+return(0);
+}
